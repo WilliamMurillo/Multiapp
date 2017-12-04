4213f22eec8c3cae9026e795525427c81d652b4d
WilliamMurillo <willystar0011@gmail.com>
2017-12-03 23:21:26 -0500

    Subiendo multiapp

---
+++ MultiAplicacion/Principal/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Principal
+{
+    [Activity(Label = "Apps", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+         ImageButton btnCalendar, btnTraslate, btnConversormoneda, btnConversorUnidades, btnPaint, btnContactos, btnCalculadora;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            
+            
+        }
+    }
+}
+
